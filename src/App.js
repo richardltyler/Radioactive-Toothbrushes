d484cf9aaf7665fdf6438652c08a1dd161a3cd28
Richard Tyler <richardtyler2011@gmail.com>
2021-02-03 14:13:58 -0700

    Remove logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 
@@ -14,18 +13,6 @@ class App extends Component {
     return (
           <div className="App">
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Yo Gabby Gabby
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
             </header>
           </div>
         );
