654545c686e4b602bbd368560ac1d17e6a95de80
Richard Tyler <richardtyler2011@gmail.com>
2021-02-03 22:21:16 -0700

    Comment changes to card image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -10,6 +10,9 @@ const Card = ({id, poster, title}) => {
         alt={title}
         id={id}
         className='card-poster'
+        // I changed this className so that we could style this image cuz the styling from the home icon in the header was messing with it
+        // I think this should be fine but just in case I don't know what I'm talking about (VERY plausible) it used to be the line below:
+        // className={id}
       />
       <h2 className='card-title'>{title}</h2>
     </article>
