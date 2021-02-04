d69e3aeacf3d85a14a01ce898fba24eea0427067
Richard Tyler <richardtyler2011@gmail.com>
2021-02-03 17:46:54 -0700

    Display Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,8 +15,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-      </div>
+      <>
+        <Header />
+        <div className="App">
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import homeButton from './home.png';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <img className='home-icon' src={homeButton} altText='home'></img>
+      <h1>Rancid Tarantulas</h1>
+    </header>
+  )
+}
+
+export default Header;
