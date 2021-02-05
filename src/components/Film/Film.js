3bb0799ef6964af23ce1af4d10306866f2dbd8c8
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-05 09:15:55 -0500

    Include comment about styling & tested getGenres

--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -5,35 +5,37 @@ class Film extends Component {
   constructor() {
     super();
     this.state = {
-      id: 1, 
-      title: "Fake Movie Title", 
-      poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
-      backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
-      release_date: "2019-12-04", 
-      overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
-      average_rating: 6, 
+      id: 1,
+      title: "Fake Movie Title",
+      poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg",
+      backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg",
+      release_date: "2019-12-04",
+      overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!",
+      average_rating: 6,
       genres: [{id: 18, name:"Drama"}],
-      // genres: [{id: 18, name:"Drama"}, {id: 18, name:"Drama"}, {id: 18, name:"Drama"}], 
+      // genres: [{id: 18, name:"Drama"}, {id: 18, name:"Drama"}, {id: 18, name:"Drama"}],
       // // getGenres() outcomes can be tested by commenting line 15 and uncommenting line 16
 
       // genres: '',
-      // // conditional rendering for gerGenres() at line 73 can be tested by 
+      // // conditional rendering for gerGenres() at line 73 can be tested by
       // // commenting lines 15 and 16 and uncommenting line 19
-      
+
       budget:63000000,
-      revenue:100853753, 
+      revenue:100853753,
       // conditional rendering of budget and revenue at lines 84 and 85
       // can be tested by commenting lines 23 and 24
-      runtime:139, 
-      tagline: "It's a movie!" 
+      runtime:139,
+      tagline: "It's a movie!"
     }
   }
 
   getGenres = () => {
     const genres = this.state.genres.map(genre => genre.name);
-  
+
     return <h4>{genres.join(', ')}</h4>
   }
+//is there a way to adjust so that the genres stay on one line rather than getting crunched together?
+
 
   formatMoney = (money) => {
     const amount = this.state[money];
@@ -55,7 +57,7 @@ class Film extends Component {
   formatDate = () => {
     const newDate = new Date(this.state.release_date).toDateString();
     const dateWithoutDay = newDate.split(' ').slice(1);
-    
+
     return <h4>{dateWithoutDay.join(' ')}</h4>
   }
 
@@ -64,7 +66,7 @@ class Film extends Component {
       <article className='single-film'>
         <section className='film-details'>
           <div className='film-title-container'>
-            <h2 className='title'>{this.state.title}</h2> 
+            <h2 className='title'>{this.state.title}</h2>
             <h3 className='rating'>{`${this.state.average_rating}/10`}</h3>
           </div>
           <h3>{this.state.tagline}</h3>
@@ -88,6 +90,6 @@ class Film extends Component {
         </article>
     )
   }
-} 
+}
 
 export default Film;
