56d6d34ed5bd54251e05b979c769f3283c4bf885
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-05 09:14:42 -0500

    Add conditional rendering based on boolean value

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,9 +10,17 @@ class App extends Component {
     super();
     this.state = {
       movies: movieData.movies,
-      currentMovie: '',
+      isMovie: false,
+      //when movie is clicked, change to true, then this.setState({isMovie: true, movieClick:event.target.id})
+      //if being from the card that was clicked
+      movieClicked: {},
+      isHome: false
       // {"movie": {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: [{id: 18, name:"Drama"}], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }}
     }
+  }
+
+  goHome = event => {
+    this.setState({isMovie: false, movieClick: {}, isHome: true})
   }
 
   selectMovie = event => {
@@ -24,9 +32,9 @@ class App extends Component {
       <>
         <Header />
         <div className="App">
-          {/* {this.state.currentMovie && <Film />}
-          <Movies movies={this.state.movies} /> */}
-          <Film />
+          {!this.state.isHome && <Film />}
+          
+          <Movies movies={this.state.movies} />
         </div>
       </>
     );
