e5ba3d8a0dd9e27679e9a054c8e35590b0bc18ff
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-05 09:14:23 -0500

    Remove onClick function for now due to error throwing conflicts

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,14 +4,12 @@ import './Card.css'
 
 const Card = ({id, poster, title}) => {
   return (
-    <article className='card' id={id} onClick={this.props.selectMovie}>
+    <article className='card' id={id}>
+    //I had to remove the onClick bc when is was even commented out, react/webpack "Read" that is was still there. 
       <img
         src={poster}
         alt={title}
         className='card-poster'
-        // I changed this className so that we could style this image cuz the styling from the home icon in the header was messing with it
-        // I think this should be fine but just in case I don't know what I'm talking about (VERY plausible) it used to be the line below:
-        // className={id}
       />
       <h2 className='card-title'>{title}</h2>
     </article>
