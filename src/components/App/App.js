25b0b61935dc18bec7cc674406f3b9764ed797d5
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-05 19:28:20 -0500

    Create goHome function to switch between hard coded film view to home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,27 +10,26 @@ class App extends Component {
     super();
     this.state = {
       movies: movieData.movies,
-      isMovie: false,
       //when movie is clicked, change to true, then this.setState({isMovie: true, movieClick:event.target.id})
       //if being from the card that was clicked
       movieClicked: {},
-      isHome: true
+      isHome: false
       // {"movie": {id: 1, title: "Fake Movie Title", poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", average_rating: 6, genres: [{id: 18, name:"Drama"}], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!" }}
     }
   }
 
-  goHome = event => {
-    this.setState({isMovie: false, movieClick: {}, isHome: true})
+  goHome = () => {
+    this.setState({ movieClicked: {}, isHome: true})
   }
 
-  selectMovie = event => {
-    this.setState({ currentMovie: event.target.id });
+  selectMovie = () => {
+    this.setState({ currentMovie: {}, isHome: false });
   }
 
   render() {
     return (
       <>
-        <Header />
+        <Header goHome={this.goHome}/>
         <div className="App">
           {!this.state.isHome
             ? <Film />
