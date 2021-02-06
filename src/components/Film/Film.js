f4e5286be3bdefd98184a2d5c70d1dfdcd6c6a74
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-06 15:22:29 -0500

    Apply UX for loading

--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -4,17 +4,21 @@ import './Film.css';
 class Film extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      currentMovie: {},
+      isLoading: true,
+      error: false
+    };
   }
 
   componentDidMount() {
     fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.currentMovie}`)
       .then(response => response.json())
-      .then(film => this.setState(film.movie))
+      .then(film => this.setState({currentMovie: film.movie, isLoading: false}))
   }
 
   getGenres = () => {
-    return <h4 className='genre-display'>{this.state.genres.join(', ')}</h4>
+    return <h4 className='genre-display'>{this.state.currentMovie.genres.join(', ')}</h4>
   }
 
   formatMoney = money => {
@@ -22,42 +26,42 @@ class Film extends Component {
 
     return (
       <>
-        <h4>{`${name}:`}</h4> 
+        <h4>{`${name}:`}</h4>
         <h5>{`${this.getNumberAmount(money)}`}</h5>
       </>
     )
   }
 
   getNumberAmount = amount => {
-    const number = this.state[amount];
+    const number = this.state.currentMovie[amount];
     const numLength = parseInt(number).toString().length;
-  
+
     if(numLength > 3 && numLength < 7) {
       const newNum = parseFloat((number/1000).toFixed(1));
       return `$${newNum}k`;
-  
+
     } else if (numLength > 6 && numLength < 10) {
       const newNum = parseFloat((number/1000000).toFixed(1));
       return `$${newNum}mil`;
-  
+
     } else if (numLength > 9) {
       const newNum = parseFloat((number/1000000000).toFixed(1));
       return `$${newNum}bil`;
-  
+
     } else {
       return `$${parseInt(number)}`
     }
   }
 
   formatDate = () => {
-    const newDate = new Date(this.state.release_date).toDateString();
+    const newDate = new Date(this.state.currentMovie.release_date).toDateString();
     const dateWithoutDay = newDate.split(' ').slice(1);
 
     return <h4>{dateWithoutDay.join(' ')}</h4>
   }
 
   formatRating = () => {
-    const rating = this.state.average_rating;
+    const rating = this.state.currentMovie.average_rating;
     const formattedRating = parseFloat(rating.toFixed(1));
 
     return <h3 className='rating'>{`${formattedRating}/10`}</h3>;
@@ -65,31 +69,37 @@ class Film extends Component {
 
   render() {
     return (
-      <article className='single-film'>
-        <section className='film-details'>
-          <div className='film-title-container'>
-            <h2 className='title'>{this.state.title}</h2>
-            {this.state.average_rating && this.formatRating()}
-          </div>
-          <h3>{this.state.tagline}</h3>
-          <div className='info-container'>
-            <h4>{`${this.state.runtime} min`}</h4>
-            {this.state.genres && this.getGenres()}
-            {this.formatDate()}
-          </div>
-        </section>
-        <img src={this.state.poster_path} alt={this.state.title} />
-        <section className='overview'>
-          <article className='summary' aria-label='summary'>
-            <h3>Summary:</h3>
-            <p>{this.state.overview}</p>
-          </article>
-          <div className='money-container'>
-            {this.state.budget > 0 && this.formatMoney('budget')}
-            {this.state.revenue > 0 && this.formatMoney('revenue')}
-          </div>
-        </section>
+      <>
+      {this.state.isLoading && <h2>Looking for your movie...</h2>}
+
+      {!this.state.isLoading &&
+        <article className='single-film'>
+          <section className='film-details'>
+            <div className='film-title-container'>
+              <h2 className='title'>{this.state.currentMovie.title}</h2>
+              {this.state.currentMovie.average_rating && this.formatRating()}
+            </div>
+            <h3>{this.state.currentMovie.tagline}</h3>
+            <div className='info-container'>
+              <h4>{`${this.state.currentMovie.runtime} min`}</h4>
+              {this.state.currentMovie.genres && this.getGenres()}
+              {this.formatDate()}
+            </div>
+          </section>
+          <img src={this.state.currentMovie.poster_path} alt={this.state.currentMovie.title} />
+          <section className='overview'>
+            <article className='summary' aria-label='summary'>
+              <h3>Summary:</h3>
+              <p>{this.state.currentMovie.overview}</p>
+            </article>
+            <div className='money-container'>
+              {this.state.currentMovie.budget > 0 && this.formatMoney('budget')}
+              {this.state.currentMovie.revenue > 0 && this.formatMoney('revenue')}
+            </div>
+          </section>
         </article>
+      }
+      </>
     )
   }
 }
