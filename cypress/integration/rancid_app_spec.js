85211d96c149693abeb5d4e077a20e5eb0f74867
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-08 17:39:44 -0500

    Create tests for Card

---
+++ cypress/integration/rancid_app_spec.js
@@ -0,0 +1,31 @@
+//What do we want to test?
+
+describe('RT App', () => {
+const baseURL = 'http://localhost:3000/';
+  //beforeEach to account for cy.visit(baseURL)
+  //header is showing
+  // how do we test how many movies there are in the API
+  describe('RT Header', () => {
+    //other stuff
+  });
+  describe('RT Film', () => {
+    //other stuff
+  });
+  describe('RT Movies', () => {
+    //other stuff
+    //is it displaying cards
+    //section?
+  });
+  describe('RT Card', () => {
+    it('Should display a movie card with an id', () => {
+      cy.visit(baseURL)
+        .get('.card').should('have.attr', 'id')
+        //how to test for functions/methods
+    })
+    it('Should have an img with alt text', () => {
+      cy.visit(baseURL)
+        .get('.card > img')
+        .get('.card > img').should('have.attr', 'alt')
+    })
+  });
+});
