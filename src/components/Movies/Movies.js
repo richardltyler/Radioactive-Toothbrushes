72f926b87adbe4bbef05dec67fcc268ff252146e
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-08 16:56:37 -0500

    Remove comments

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -26,11 +26,7 @@ const Movies = ({movies, selectMovie}) => {
 
 export default Movies;
 
-//we first call upon the component and the props being passed into it.
 Movies.propTypes = {
-  //then we access the prop/name we're passing in and assign that as the key with the value being an argument/check that the prop is the data type we're expecting or want
   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-  //if the data type isn't what we expect or want, an error will be thrown that tells us ie: "we expected a string, but got a number"
-  //to test this, please change the value of movies to PropTypes.arrayOf(PropTypes.string) rather than object
   selectMovie: PropTypes.func.isRequired
 }
