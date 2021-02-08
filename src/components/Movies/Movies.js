8d01d4823a7b90a104979c281172f0763512c188
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-08 08:14:54 -0500

    Add propTypes to Movies to ensure data being passed is what's expected

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Movies.css'
 import Card from '../Cards/Cards'
 
@@ -24,3 +25,12 @@ const Movies = ({movies, selectMovie}) => {
 }
 
 export default Movies;
+
+//we first call upon the component and the props being passed into it.
+Movies.propTypes = {
+  //then we access the prop/name we're passing in and assign that as the key with the value being an argument/check that the prop is the data type we're expecting or want
+  movies: PropTypes.arrayOf(PropTypes.object),
+  //if the data type isn't what we expect or want, an error will be thrown that tells us ie: "we expected a string, but got a number"
+  //to test this, please change the value of movies to PropTypes.arrayOf(PropTypes.string) rather than object
+  selectMovie: PropTypes.func
+}
