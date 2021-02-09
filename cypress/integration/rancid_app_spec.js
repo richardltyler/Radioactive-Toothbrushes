ec3d9f0e26023516ae993d796bc8c984ed27dcd0
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-09 08:54:39 -0500

    Build App testing for loading UX and if Header & Movies is visible

--- cypress/integration/rancid_app_spec.js
+++ cypress/integration/rancid_app_spec.js
@@ -7,14 +7,26 @@ describe('Radioactive Toothbrushes', () => {
   });
 
   describe('App', () => {
+    it('Should display a header', () => {
+      cy.get('header').should('be.visible')
+    });
+
+    it('Should display a loading message', () => {
+      cy.get('div').contains('Please wait...')
+    })
+
+    //how do we test API isn't functioning?
+    // it('Should display an error message', () => {
+    //   cy.get('div').contains('We are having a technical difficulty')
+    // })
+
     // how do we test how many movies there are in the API
+    it('Should display the listed movies', () => {
+      cy.get('div > section').should('be.visible')
+    })
   });
 
   describe('RT Header', () => {
-    it('Should display a header', () => {
-      cy.get('header')
-    });
-
     it('Should have a title/logo', () => {
       cy.get('h1').contains('Radioactive Toothbrushes')
     });
@@ -24,10 +36,12 @@ describe('Radioactive Toothbrushes', () => {
         .contains('Mulan')
         .click()
       cy.get('header > img').click();
-    })
+    });
   });
 
   describe('RT Film', () => {
+    //how do we test that API is functioning?
+    //how do we test API isn't functioning?
     it('Should be able to display a single movie\'s details', () => {
       //set up to access Film
       cy.get('section > article')
@@ -39,7 +53,7 @@ describe('Radioactive Toothbrushes', () => {
         .get('article > img').should('be.visible')
         .get('article > section > article > h3').contains('Summary')
         .get('article > section > article > h3').contains('Summary')
-    })
+    });
   });
 
   describe('RT Movies', () => {
@@ -47,21 +61,23 @@ describe('Radioactive Toothbrushes', () => {
       cy.get('.movies-container').should(($article) => {
         expect($article).to.have.length
         expect($article.first()).to.contain('Money Plane')
-      })
-    })
+      });
+    });
+
     it('Should contain clickable movie cards', () =>{
       cy.get('.movies-container > article').contains('Money Plane').click()
-    })
+    });
   });
 
   describe('RT Card', () => {
     it('Should display a movie card with an id', () => {
         cy.get('.card').should('have.attr', 'id')
         //how to test for functions/methods
-    })
+    });
+
     it('Should have an img with alt text', () => {
         cy.get('.card > img')
           .get('.card > img').should('have.attr', 'alt')
-    })
+    });
   });
 });
