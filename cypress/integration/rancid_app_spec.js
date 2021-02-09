9dccb8dbe62498b9ff6feda970f62778b92310a0
Richard Tyler <richardtyler2011@gmail.com>
2021-02-08 18:21:26 -0700

    Test Header

--- cypress/integration/rancid_app_spec.js
+++ cypress/integration/rancid_app_spec.js
@@ -1,31 +1,51 @@
 //What do we want to test?
 //describe('RadioActive')
-describe('RT App', () => {
-const baseURL = 'http://localhost:3000/';
-  //beforeEach to account for cy.visit(baseURL)
-  //header is showing
-  // how do we test how many movies there are in the API
-  describe('RT Header', () => {
-    //other stuff
+
+describe('Radioactive Toothbrushes', () => {
+
+  const baseURL = 'http://localhost:3000/';
+
+  beforeEach(() => {
+    cy.visit(baseURL);
   });
+ 
+  // how do we test how many movies there are in the API
+  describe('App', () => {
+
+  });
+
+  describe('RT Header', () => {
+    it('Should display a header', () => {
+      cy.get('header')
+    });
+
+    it('Should have a title/logo', () => {
+      cy.get('h1').contains('Radioactive Toothbrushes')
+    });
+
+    it('Should have a clickable home icon', () => {
+      cy.get('img').click();
+    })
+  });
+
   describe('RT Film', () => {
     //other stuff
   });
+
   describe('RT Movies', () => {
     //other stuff
     //is it displaying cards
     //section?
   });
+
   describe('RT Card', () => {
     it('Should display a movie card with an id', () => {
-      cy.visit(baseURL)
-        .get('.card').should('have.attr', 'id')
+        cy.get('.card').should('have.attr', 'id')
         //how to test for functions/methods
     })
     it('Should have an img with alt text', () => {
-      cy.visit(baseURL)
-        .get('.card > img')
-        .get('.card > img').should('have.attr', 'alt')
+        cy.get('.card > img')
+          .get('.card > img').should('have.attr', 'alt')
     })
   });
 });
