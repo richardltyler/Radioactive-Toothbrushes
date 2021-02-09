541ad951646a7b64e45ab35069d13f7192b11e72
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-09 18:11:43 -0500

    Add indentation

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -4,8 +4,8 @@ import './Error.css';
 const Error = () => {
     return (
       <div className='error-message'>
-      <img src='https://media.giphy.com/media/zWIZO0saJg4g/giphy.gif' alt='trying to squeeze toothpaste on a toothbrush and making a mess instead' />
-      <h2>We're having some technical difficulties.</h2>
+        <img src='https://media.giphy.com/media/zWIZO0saJg4g/giphy.gif' alt='trying to squeeze toothpaste on a toothbrush and making a mess instead' />
+        <h2>We're having some technical difficulties.</h2>
       </div>
     )
 };
