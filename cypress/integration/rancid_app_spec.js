eb4d70a3e7ecaede1fd77cc634ddb3c8cff7d5d4
Richard Tyler <richardtyler2011@gmail.com>
2021-02-10 16:57:42 -0700

    Refactor tests to intercept api to dummy data

--- cypress/integration/rancid_app_spec.js
+++ cypress/integration/rancid_app_spec.js
@@ -1,49 +1,100 @@
-describe('Radioactive Toothbrushes', () => {
+import { elementType } from "prop-types";
 
+describe('Radioactive Toothbrushes', () => {
   const baseURL = 'http://localhost:3000/';
 
-  beforeEach(() => {
-    cy.visit(baseURL);
-  });
 
   describe('App', () => {
+      beforeEach(() => {
+        cy.fixture('Movies-data.json')
+          .then(movies => {
+            cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+              body: movies
+            })
+          });
+
+          cy.visit(baseURL);
+      });
+      
     it('Should display a header', () => {
+      
       cy.get('header').should('be.visible')
     });
-
+    
     it('Should display a loading message', () => {
       cy.get('div').contains('Please wait...')
     })
-
+    
     //how do we test API isn't functioning?
     // it('Should display an error message', () => {
-    //   cy.get('div').contains('We are having a technical difficulty')
-    // })
-
-    // how do we test how many movies there are in the API
-    it('Should display the listed movies', () => {
-      cy.get('.movies-container')
+      //   cy.get('div').contains('We are having a technical difficulty')
+      // })
+      
+      // how do we test how many movies there are in the API
+      it('Should display the listed movies', () => {
+        
+       
+        cy.get('.movies-container')
         .should('be.visible')
+      });
     });
-  });
-
+    
   describe('RT Header', () => {
+    beforeEach(() => {
+
+      cy.fixture('Film-data.json')
+        .then(movie => {
+          cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
+            body: movie
+          })
+        })
+
+      cy.fixture('Movies-data.json')
+        .then(movies => {
+          cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+            body: movies
+          })
+        });
+
+      cy.visit(baseURL);
+    });
+
     it('Should have a title/logo', () => {
       cy.get('h1').contains('Radioactive Toothbrushes')
     });
 
     it('Should click home icon to navigate back to main', () => {
-      cy.get('section > article')
-        .contains('Mulan')
+      cy.get('section > a') 
+        .contains('Mulan')     
         .click()
-      cy.get('header > img').click();
+
+      cy.get('header > a').click();
     });
   });
 
   describe('RT Film', () => {
+    beforeEach(() => {
+
+      cy.fixture('Film-data.json')
+        .then(movie => {
+          cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401', {
+            body: movie
+          })
+        })
+
+      cy.fixture('Movies-data.json')
+        .then(movies => {
+          cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+            body: movies
+          })
+        });
+
+      cy.visit(baseURL);
+    });
+
     it('Should have a loading message', () => {
       //set up to access Film
-      cy.get('section > article')
+      cy.get('section > a')
         .contains('Mulan')
         .click()
         //inside of Film
@@ -61,7 +112,7 @@ describe('Radioactive Toothbrushes', () => {
     //how do we test that API is functioning?
     it('Should be able to display a single movie\'s details', () => {
       //set up to access Film
-      cy.get('section > article')
+      cy.get('section > a')
         .contains('Mulan')
         .click()
         //inside of Film
@@ -75,6 +126,24 @@ describe('Radioactive Toothbrushes', () => {
   });
 
   describe('RT Movies', () => {
+    beforeEach(() => {
+      cy.fixture('Film-data.json')
+        .then(movie => {
+          cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
+            body: movie
+          })
+        })
+
+      cy.fixture('Movies-data.json')
+        .then(movies => {
+          cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+            body: movies
+          })
+        });
+
+      cy.visit(baseURL);
+    });
+
     it('Should display many movie cards', () => {
       cy.get('.movies-container').should(($article) => {
         expect($article).to.have.length
@@ -83,11 +152,22 @@ describe('Radioactive Toothbrushes', () => {
     });
 
     it('Should contain clickable movie cards', () =>{
-      cy.get('.movies-container > article').contains('Money Plane').click()
+      cy.get('.movies-container > a').contains('Money Plane').click()
     });
   });
 
   describe('RT Card', () => {
+    beforeEach(() => {
+      cy.fixture('Movies-data.json')
+        .then(movies => {
+          cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+            body: movies
+          })
+        });
+      
+      cy.visit(baseURL);
+    });
+
     it('Should display a movie card with an id', () => {
         cy.get('.card').should('have.attr', 'id')
         //how to test for functions/methods
