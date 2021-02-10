2225bc4d2db8ab34f70237e7c8319e5b16e133e1
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2021-02-10 16:29:42 -0500

    Remove semicolon

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,13 +1,13 @@
 const apiCalls = {
   getAllMovies() {
-    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies/');
+    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies/')
   },
 
   getOneMovie(id) {
     return (
       fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
         .then(response => response.json())
-    );
+    )
   }
 }
 
