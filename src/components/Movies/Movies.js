2e29fd463751b5a87dfb86986778180ddcb37634
Richard Tyler <richardtyler2011@gmail.com>
2021-02-11 15:44:51 -0700

    Add semicolons in Movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Movies.css'
-import Card from '../Cards/Cards'
+import './Movies.css';
+import Card from '../Cards/Cards';
 
 const Movies = ({ movies }) => {
 
@@ -13,18 +13,18 @@ const Movies = ({ movies }) => {
         poster={movie.poster_path}
         title={movie.title}
       />
-    )
-  })
+    );
+  });
 
   return (
     <section className='movies-container'>
       {movieCards}
     </section>
-  )
+  );
 }
 
 export default Movies;
 
 Movies.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+};
